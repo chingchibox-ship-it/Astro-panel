ff898b72d387ecaabc243019818a955af03629ee
chingchibox-ship-it <chingchibox@gmail.com>
2026-07-04 01:53:03 +0500

    Delete setup-panel.sh

--- setup-panel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -f /usr/sbin/pufferpanel ]; then
-    curl -s https://packagecloud.io | sudo bash
-    sudo apt-get update && sudo apt-get install pufferpanel -y
-    sudo pufferpanel user add --name admin --email admin@example.com --password password123 --admin
-fi
-sudo /usr/sbin/pufferpanel start
